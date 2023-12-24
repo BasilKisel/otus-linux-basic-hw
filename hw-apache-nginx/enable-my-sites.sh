74e388c0a2dcf1550e774ba0792880e5c4f227fb
kisel_vs <basilix@yandex.ru>
2023-12-24 19:58:55 +0000

    hw-apache-nginx: kicked apach2 to work.

---
+++ hw-apache-nginx/enable-my-sites.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script makes shure the homework will be run at all costs.
+
+# Redefine Apache2's ports.config for homework
+if [ ! -e /etc/apache2/ports.conf.bac ]
+then
+    mv /etc/apache2/ports.conf /etc/apache2/ports.conf.bac
+    ln ./ports.conf /etc/apache2/ports.conf
+fi
+
+# Since security is really concert with HTTPD, I'll map my index.html to Apache2's data dir
+mkdir /var/www/html8081
+ln -f ./html8081/index.html /var/www/html8081/index.html
+mkdir /var/www/html8082
+ln -f ./html8082/index.html /var/www/html8082/index.html
+mkdir /var/www/html8083
+ln -f ./html8083/index.html /var/www/html8083/index.html
+
+# Activates HW's "my-sites" config
+ln -f ./my-sites.conf /etc/apache2/sites-available/my-sites.conf
+a2ensite my-sites
+systemctl enable apache2
+systemctl start apache2
+systemctl reload apache2
