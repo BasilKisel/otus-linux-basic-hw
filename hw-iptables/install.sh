0773b024832ab2613109986e651ddc48e30b631a
kisel_vs <basilix@yandex.ru>
2024-01-25 20:42:34 +0000

    iptables - updated results.

--- hw-iptables/install.sh
+++ hw-iptables/install.sh
@@ -8,7 +8,7 @@ fi
 
 if `dpkg-query --status iptables-persistent | grep -s -e "Status:" | grep -q -v -e "\binstalled\b"`
 then
-    echo "$0: Preparing to install iptables-save, iptables-restore utilities"
+    echo "$0: Preparing to install iptables-save, iptables-restore, and iptables-apply utilities"
     echo "$0: Please, leave default filepaths to rules intact to use 'fortify.sh' script."
     read
 
