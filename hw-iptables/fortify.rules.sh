447c8de0cb4ed9732899ad406cf5cec26adfbfc9
kisel_vs <basilix@yandex.ru>
2024-01-25 20:20:11 +0000

    iptables - Added conntrack.

--- hw-iptables/fortify.rules.sh
+++ hw-iptables/fortify.rules.sh
@@ -4,5 +4,6 @@
 
 iptables -t filter -F INPUT
 iptables -t filter -A INPUT -s 127.0.0.1 -d 127.0.0.1 -j ACCEPT # A test showed 127.0.0.1 didn't work while localhost did.
+iptables -t filter -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT # To turn in this homework :)
 iptables -t filter -A INPUT -p tcp -m multiport --dports 80,22,443,3306,8081 -j ACCEPT
 iptables -t filter -P INPUT DROP
