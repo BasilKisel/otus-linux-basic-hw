fd168932f87a599cf220e7adf4b26f6907bf5d49
kisel_vs <basilix@yandex.ru>
2024-01-25 20:36:11 +0000

    iptables - worked out DNS problem.

--- hw-iptables/fortify.rules.sh
+++ hw-iptables/fortify.rules.sh
@@ -4,6 +4,7 @@
 
 iptables -t filter -F INPUT
 iptables -t filter -A INPUT -s 127.0.0.1 -d 127.0.0.1 -j ACCEPT # A test showed 127.0.0.1 didn't work while localhost did.
-iptables -t filter -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT # To turn in this homework :)
-iptables -t filter -A INPUT -p tcp -m multiport --dports 80,22,443,3306,8081 -j ACCEPT
+iptables -t filter -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT # To let out connections be carring out.
+iptables -t filter -A INPUT -p tcp -m multiport --dports 80,22,443,53,853,3306,8081 -j ACCEPT
+iptables -t filter -A INPUT -p udp --dport 53 -j ACCEPT # DNS answers
 iptables -t filter -P INPUT DROP
