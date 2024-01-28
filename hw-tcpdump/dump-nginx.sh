80998a25d7e0b64212341bdc95f39d63773a1e44
Basil Kisel <basilix@yandex.ru>
2024-01-28 14:18:17 +0000

    tcpdump - replaced human readable dump with pcap file.

--- hw-tcpdump/dump-nginx.sh
+++ hw-tcpdump/dump-nginx.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-./dump-host-port.sh 192.168.1.8 80 > './192.168.1.8_80.tcpdump'
-chmod 666 './192.168.1.8_80.tcpdump'
+# This script dumps a response from 192.168.1.8:80 into a file.
+
+host=192.168.1.8
+port=80
+outfile='./192.168.1.8_80.pcap' 
+
+# Sudo permissions are needed to capture on network card.
+if [ `id -u` != 0 ]
+then
+    echo "$0: One must be root to launch this script. Bye!"
+    exit 2
+fi
+
+tcpdump "port $port and host $host" -n -vvv -w "$outfile" &
+sleep 1 # Wait for tcpdump to start
+curl -s "$host:$port" >/dev/null 2>&1
+sleep 2 # Gracious period for tcpdump to print out captured packages.
+kill %1
+
