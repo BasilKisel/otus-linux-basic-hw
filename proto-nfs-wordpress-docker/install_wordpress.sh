ad4e00b71ae5fc5d4180074933b575e6991b3cb6
Basil Kisel <basilix@yandex.ru>
2024-02-07 16:23:43 +0000

    nfs - finished WP install script

--- proto-nfs-wordpress-docker/install_wordpress.sh
+++ proto-nfs-wordpress-docker/install_wordpress.sh
@@ -4,19 +4,21 @@
 # This script downloads, installs and configures Wordpress 6.4.3.
 
 
-wordpress_tgz_url='https://wordpress.org/wordpress-6.4.3.tar.gz'
-wordpress_file_tgz='./wordpress-6.4.3.tar.gz'
+wp_tgz_url='https://wordpress.org/wordpress-6.4.3.tar.gz'
+wp_file_tgz='./wordpress-6.4.3.tar.gz'
 target_dir='/var/nfs_data/www-data'
+wp_config_path="$target_dir/wordpress/wp-config.php"
+wp_sample_config_path="$target_dir/wordpress/wp-config-sample.php"
+wp_def_config='./wp-config.php'
 
 
 # Get Wordpress into NFS share
 
-if [ ! -e "$wordpress_file_tgz" ]
-then
-    curl -s -o "$wordpress_file_tgz" "$wordpress_tgz_url" 
-fi
-tar xzf "$wordpress_file_tgz" --one-top-level="$target_dir" --skip-old-files --no-overwrite-dir
+[ -e "$wp_file_tgz" ] || curl -s -o "$wp_file_tgz" "$wp_tgz_url" 
+tar xzf "$wp_file_tgz" -C "$target_dir/" --skip-old-files
 
 
-# Configure DBMS connection
+# Configure DBMS connection and secret keys
 
+[ -e "$wp_config_path" ] || cp "$wp_def_config" "$wp_config_path"
+[ ! -e "$wp_sample_config_path" ] || rm "$wp_sample_config_path"
