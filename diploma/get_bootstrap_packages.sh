2faf71e2bdf170e263bb66977755ceab451aea31
Василий Кисель <v.kisel@csat.ru>
2024-02-14 19:13:30 +0300

    diploma - bootstrap - added package script.

---
+++ diploma/get_bootstrap_packages.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+# This script downloads DEB packages into a bootstrap VM image.
+
+
+if [ `id -u` != 0 ]
+then
+    echo "$0: One must be root to start this script."
+    exit 1
+fi
+
+# all nodes
+## metrics
+apt-get install --download-only -y prometheus-node-exporter
+## firewall
+apt-get install --download-only -y iptables-persistent
+## admin files, web-app files
+apt-get install --download-only -y nfs-common
+
+# reverse proxy
+apt-get install --download-only -y nginx prometheus-nginx-exporter
+# web-app
+apt-get install --download-only -y docker.io
+# dbms
+apt-get install --download-only -y mysql-server-8.0
+# file share
+apt-get install --download-only -y nfs-kernel-server
+# elk
+apt-get install --download-only -y default-jdk
